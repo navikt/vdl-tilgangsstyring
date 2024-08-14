1d6a33764aebfa24e3a89dcaf849e06e1318d33a
emilieeo <emilie.eliseussen.odegaard@nav.no>
2024-08-14 15:14:16 +0200

    Lagt til form for input fra user

--- tilgangsstyring/grupper.py
+++ tilgangsstyring/grupper.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+from snowflake.connector.errors import ProgrammingError
 from snowflake.snowpark.context import get_active_session
 
 # Get the current credentials
@@ -12,49 +13,64 @@ df = session.sql(query)
 queried_data = df.to_pandas()
 st.table(data=queried_data)
 #queried_data = df.to_pandas()
-#st.dataframe(pd.DataFrame(df, columns=["Gruppe", "Gruppebeskrivelse"]), hide_index=True)
+#st.dataframe(queried_data, columns=["Gruppe", "Gruppebeskrivelse"], hide_index=True)
 
 
 if st.button("+ Lag ny gruppe"):
 
-    
     st.title("Lag ny gruppe")
-    gruppenavn = st.text_input("Gruppenavn")
-    gruppe_beskrivelse = st.text_input("Gruppebeskrivelse")
+    with st.form("new_member_form"):
+        gruppenavn = st.text_input("Gruppenavn")
+        gruppe_beskrivelse = st.text_input("Gruppebeskrivelse")
 
-    if st.button("Send inn"):
-        query = f"""
-            MERGE INTO grupper old USING (
-                SELECT 
-                    '{gruppenavn}' as gruppe,
-                    '{gruppe_beskrivelse}' as gruppe_beskrivelse,
-                    current_user as _opprettet_av,
-                    current_date as _opprettet_dato, 
-                    current_user as _oppdatert_av,
-                    current_date as _oppdatert_dato 
-                ) new_row ON new_row.gruppe = old.gruppe
-            WHEN MATCHED AND old._slettet_dato IS NULL THEN UPDATE SET 
-                old.gruppe_beskrivelse = new_row.gruppe_beskrivelse, 
-                old._oppdatert_av = new_row._oppdatert_av,
-                old._oppdatert_dato = new_row._oppdatert_dato 
-            WHEN NOT MATCHED THEN INSERT (
-                old.gruppe, 
-                old.gruppe_beskrivelse,
-                old._opprettet_av,
-                old._opprettet_dato, 
-                old._oppdatert_av,
-                old._oppdatert_dato
-            ) VALUES (
-                new_row.gruppe, 
-                new_row.gruppe_beskrivelse,
-                new_row._opprettet_av,
-                new_row._opprettet_dato, 
-                new_row._oppdatert_av,
-                new_row._oppdatert_dato
-            )
-        """
-        session.sql(query)
+        if st.form_submit_button("Send inn"):
+            query = f"""
+                MERGE INTO grupper old USING (
+                    SELECT 
+                        '{gruppenavn}' as gruppe,
+                        '{gruppe_beskrivelse}' as gruppe_beskrivelse,
+                        current_user as _opprettet_av,
+                        current_date as _opprettet_dato, 
+                        current_user as _oppdatert_av,
+                        current_date as _oppdatert_dato, 
+                        NULL as _slettet_dato
+                    ) new_row ON new_row.gruppe = old.gruppe
+                WHEN MATCHED AND old._slettet_dato IS NULL THEN UPDATE SET 
+                    old.gruppe_beskrivelse = new_row.gruppe_beskrivelse, 
+                    old._oppdatert_av = new_row._oppdatert_av,
+                    old._oppdatert_dato = new_row._oppdatert_dato 
+                WHEN NOT MATCHED THEN INSERT (
+                    old.gruppe, 
+                    old.gruppe_beskrivelse,
+                    old._opprettet_av,
+                    old._opprettet_dato, 
+                    old._oppdatert_av,
+                    old._oppdatert_dato, 
+                    old._slettet_dato
+                ) VALUES (
+                    new_row.gruppe, 
+                    new_row.gruppe_beskrivelse,
+                    new_row._opprettet_av,
+                    new_row._opprettet_dato, 
+                    new_row._oppdatert_av,
+                    new_row._oppdatert_dato, 
+                    new_row._slettet_dato
+                )
+            """
+            st.session_state
+            st.write(query)
+            st.markdown(query)
+            session.run_query(query)
+            session.sql(query)
+            try:
+                st.markdown("hei")
+                session.sql(query)
+                st.success("Suksess: medlem lagt til")
+                #st.session_state.page = "home"
+                st.experimental_rerun()  # Return to home page after submission
+            except ProgrammingError as e:
+                st.error(f"Error executing query: {e}")
 
-        # TODO: refresh knapp?
+
 
 
