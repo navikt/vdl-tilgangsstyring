7785279f1669ee2f168cb628ebd02a79ba0d3552
Ivar Framnes <ivar.william.kvale.framnes@nav.no>
2024-08-15 15:26:38 +0200

    kjører men endel som gjenstår

--- tilgangsstyring/kostnadsstedes_relasjon.py
+++ tilgangsstyring/kostnadsstedes_relasjon.py
@@ -27,7 +27,7 @@ with st.form("Legg til kostnadsstedsrelasjon"):
         """
     df_available_groups = session.sql(available_groups_statement).to_pandas()
     available_cost_centre_statement = f"""
-            SELECT kostnadssted_navn FROM gyldig_kostnadssted_liste
+            SELECT kostnadssted FROM gyldig_kostnadssted_liste
         """
     df_available_cost_centre = session.sql(available_cost_centre_statement).to_pandas()
     group = st.selectbox("Gyldige grupper",df_available_groups)
@@ -37,10 +37,9 @@ with st.form("Legg til kostnadsstedsrelasjon"):
 if submit_group:
     exists_statement = f"""
         SELECT * 
-        FROM gruppe_kostnadssted_relasjoner grp 
-        JOIN gyldig_kostnadssted_liste ksted on ksted.kostnadssted = grp.kostnadssted 
+        FROM gruppe_kostnadssted_relasjoner 
         WHERE gruppe = upper('{group}') 
-          AND kostnadssted_navn = '{cost_centre}' 
+          AND kostnadssted = '{cost_centre}' 
           AND _slettet_dato is null 
     """
     df_exists = session.sql(exists_statement).to_pandas()
@@ -55,11 +54,7 @@ if submit_group:
                 _oppdatert_dato
             ) VALUES (
                 upper('{group}'),
-                (  
-                    select max(kostnadssted) 
-                    from gyldig_kostnadssted_liste 
-                    where kostnadssted_navn = '{cost_centre}'
-                ),
+                '{cost_centre}',
                 '{st.experimental_user["email"]}',
                 current_date, 
                 '{st.experimental_user["email"]}',
@@ -75,33 +70,29 @@ if submit_group:
 with st.form("Slett Gruppe"):
     available_groups_statement = f"""
             SELECT gruppe 
-            FROM gruppemedlemskap 
+            FROM gruppe_kostnadssted_relasjoner 
             WHERE _slettet_dato is null 
-              AND current_date between fra_dato and til_dato
         """
     df_available_groups = session.sql(available_groups_statement).to_pandas()
-    available_groups = [row[0] for row in df_available_groups]    
     group = st.selectbox("ledig grupper",df_available_groups)
-    available_emails_statement = f"""
-            SELECT epost 
-            FROM gruppemedlemskap
+    available_cost_centre_statement = f"""
+            SELECT kostnadssted 
+            FROM gruppe_kostnadssted_relasjoner
             WHERE _slettet_dato is null 
               AND gruppe = '{group}'
-              AND current_date between fra_dato and til_dato
         """
-    df_available_emails = session.sql(available_emails_statement).to_pandas()
-    available_emails = [row[0] for row in df_available_emails]    
-    email = st.selectbox("ledig eposter",df_available_emails)
-    delete_group = st.form_submit_button('Slett gruppemedlemskap')
+    df_available_cost_centre_statement = session.sql(available_cost_centre_statement).to_pandas()
+    cost_centre = st.selectbox("ledig kostnadssteder",df_available_cost_centre_statement)
+    delete_group = st.form_submit_button('Slett kostnadsstedsrelasjon')
 
 if delete_group:
     delete_statment=f"""
-        update gruppemedlemskap set 
+        update gruppe_kostnadssted_relasjoner set 
             _oppdatert_av = '{st.experimental_user["email"]}',
             _oppdatert_dato = current_date, 
             _slettet_dato = current_date
         where gruppe = upper('{group}')
-        and epost = lower('{email}')
+        and kostnadssted = '{cost_centre}'
         """
     session.sql(delete_statment).collect()
     st.success('Success!', icon="✅")  
