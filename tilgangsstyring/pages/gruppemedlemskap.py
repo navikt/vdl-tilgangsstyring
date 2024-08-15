e6ec6d8268d39c3ecc79821fe105d91ad8bcbf91
emilieeo <emilie.eliseussen.odegaard@nav.no>
2024-08-15 13:33:41 +0200

    prøve å få gruppemedlemskap opp å gå

--- tilgangsstyring/pages/gruppemedlemskap.py
+++ tilgangsstyring/pages/gruppemedlemskap.py
@@ -7,60 +7,91 @@ from snowflake.snowpark.context import get_active_session
 session = get_active_session()
 
 st.title("Gruppemedlemskap")
-gruppe_view = f"""SELECT gruppe, epost FROM grupper WHERE _slettet_dato IS NULL"""
+gruppe_view = f"""SELECT gruppe, epost FROM gruppemedlemskap WHERE _slettet_dato IS NULL and current_date between fra_dato and til_dato"""
 df_groups = session.sql(gruppe_view).to_pandas()
 st.dataframe(df_groups, on_select="rerun", hide_index=True, use_container_width=True)
 
-with st.form("Legg til gruppe"):
-    group = st.text_input('Gruppenavn:')
-    group_desc = st.text_input('Gruppebeskrivelse:')
-    submit_group = st.form_submit_button('Legg til')
-
-if submit_group:
-        exists_statement = f"""
-            SELECT * FROM grupper WHERE gruppe = upper('{group}') and _slettet_dato is null
-        """
-        df_exists = session.sql(exists_statement).to_pandas()
-        if df_exists.empty:
-            insert_statment=f"""
-                INSERT INTO grupper (
-                    gruppe,
-                    gruppe_beskrivelse,
-                    _opprettet_av,
-                    _opprettet_dato, 
-                    _oppdatert_av,
-                    _oppdatert_dato
-                ) 
-                VALUES (
-                    upper('{group}'),
-                    '{group_desc}',
-                    '{st.experimental_user["email"]}',
-                    current_date, 
-                    '{st.experimental_user["email"]}',
-                    current_date 
-                )"""
-            session.sql(insert_statment).collect()
-            st.success('Success!', icon="✅")    
-        else:
-            st.success('Already exists')
-        st.rerun()
-
-with st.form("Slett Gruppe"):
+with st.form("Legg til medlem"):
     available_groups_statement = f"""
             SELECT gruppe FROM grupper WHERE _slettet_dato is null
         """
     df_available_groups = session.sql(available_groups_statement).to_pandas()
     available_groups = [row[0] for row in df_available_groups]
     group = st.selectbox("ledig grupper",df_available_groups)
-    delete_group = st.form_submit_button('Slett grupppe')
+    email = st.text_input("E-post")
+    from_date = st.date_input("Fra dato")
+    to_date = st.date_input("Til dato")
+    submit_group = st.form_submit_button('Legg til')
+
+if submit_group:
+        st.success(st.markdown(from_date.isoformat))
+        #exists_statement = f"""
+        #    SELECT * 
+        #    FROM gruppemedlemskap
+        #    WHERE gruppe = upper('{group}') 
+        #      AND epost = lower('{email}') 
+        #      AND _slettet_dato is null 
+        #      AND current_date between fra_dato and til_dato
+        #"""
+        #df_exists = session.sql(exists_statement).to_pandas()
+        #if df_exists.empty:
+        #    insert_statment=f"""
+        #        INSERT INTO gruppemedlemskap (
+        #            gruppe,
+        #            epost,
+        #            fra_dato, 
+        #            til_dato,
+        #            _opprettet_av,
+        #            _opprettet_dato, 
+        #            _oppdatert_av,
+        #            _oppdatert_dato
+        #        ) 
+        #        VALUES (
+        #            upper('{group}'),
+        #            lower('{email}') ,
+        #            {from_date},
+        #            {to_date},
+        #            '{st.experimental_user["email"]}',
+        #            current_date, 
+        #            '{st.experimental_user["email"]}',
+        #            current_date 
+        #        )"""
+        #    session.sql(insert_statment).collect()
+        #    st.success('Success!', icon="✅")    
+        #else:
+        #    st.success('Already exists')
+        #st.rerun()
+
+with st.form("Slett Gruppe"):
+    available_groups_statement = f"""
+            SELECT gruppe 
+            FROM gruppemedlemskap 
+            WHERE _slettet_dato is null 
+              AND current_date between fra_dato and til_dato
+        """
+    df_available_groups = session.sql(available_groups_statement).to_pandas()
+    available_groups = [row[0] for row in df_available_groups]    
+    group = st.selectbox("ledig grupper",df_available_groups)
+    available_emails_statement = f"""
+            SELECT epost 
+            FROM gruppemedlemskap
+            WHERE _slettet_dato is null 
+              AND gruppe = '{group}'
+              AND current_date between fra_dato and til_dato
+        """
+    df_available_emails = session.sql(available_emails_statement).to_pandas()
+    available_emails = [row[0] for row in df_available_emails]    
+    email = st.selectbox("ledig eposter",df_available_emails)
+    delete_group = st.form_submit_button('Slett gruppemedlemskap')
 
 if delete_group:
     delete_statment=f"""
-        update grupper set 
+        update gruppemedlemskap set 
             _oppdatert_av = '{st.experimental_user["email"]}',
             _oppdatert_dato = current_date, 
             _slettet_dato = current_date
         where gruppe = upper('{group}')
+        and epost = lower('{email}')
         """
     session.sql(delete_statment).collect()
     st.success('Success!', icon="✅")  
