637b5d5a456d746a10d0e6993e5846436aa7aa03
Ivar Framnes <ivar.william.kvale.framnes@nav.no>
2024-08-15 08:50:49 +0200

    rettet opp feil på definisjonene

--- tilgangsstyring/tilgangsstyring_strukturer.sql
+++ tilgangsstyring/tilgangsstyring_strukturer.sql
@@ -6,32 +6,32 @@ create table if not exists grupper(
     gruppe varchar(200),
     gruppe_beskrivelse varchar(1000),
     _opprettet_av varchar(40),
-    _opprettet_date date,
+    _opprettet_dato date,
     _oppdatert_av varchar(40),
-    _oppdatert_date date,
-    _slettet_date date
+    _oppdatert_dato date,
+    _slettet_dato date
 );
 
-create table if not exists gruppe_kostnadssteds_relasjoner(
+create table if not exists gruppe_kostnadssted_relasjoner(
     gruppe varchar(200),
     kostandssteder varchar(200),
     _opprettet_av varchar(40),
-    _opprettet_date date,
+    _opprettet_dato date,
     _oppdatert_av varchar(40),
-    _oppdatert_date date,
-    _slettet_date date
+    _oppdatert_dato date,
+    _slettet_dato date
 );
 
-create table if not exists gruppemeldemskap(
+create table if not exists gruppemedlemskap(
     gruppe varchar(200),
     epost varchar(200),
     fra_dato date,
     til_dato date,
     _opprettet_av varchar(40),
-    _opprettet_date date,
+    _opprettet_dato date,
     _oppdatert_av varchar(40),
-    _oppdatert_date date,
-    _slettet_date date
+    _oppdatert_dato date,
+    _slettet_dato date
 );
 
 create or replace view gyldig_kostnadssted_liste as (
