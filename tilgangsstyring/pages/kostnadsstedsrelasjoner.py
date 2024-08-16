afdc4bfe57670f855151e6c399ad734e9884bd73
emilieeo <emilie.eliseussen.odegaard@nav.no>
2024-08-16 10:58:49 +0200

    fikse sånn at varslinger fungerer i appen

--- tilgangsstyring/pages/kostnadsstedsrelasjoner.py
+++ tilgangsstyring/pages/kostnadsstedsrelasjoner.py
@@ -3,100 +3,86 @@ import pandas as pd
 from snowflake.connector.errors import ProgrammingError
 from snowflake.snowpark.context import get_active_session
 
-
 # Get the current credentials
 session = get_active_session()
 
-st.title("Kostnadsstedsrelasjoner")
+st.title("Grupper")
+left, right = st.columns(2)
 
-gruppe_view = f"""
-    SELECT 
-        grp.gruppe, 
-        grp.kostnadssted,
-        ksted.kostnadssted_navn
-    FROM gruppe_kostnadssted_relasjoner grp 
-    JOIN gyldig_kostnadssted_liste ksted on ksted.kostnadssted = grp.kostnadssted 
-    WHERE _slettet_dato IS NULL 
+
+right.write(
     """
-df_groups = session.sql(gruppe_view).to_pandas()
-st.dataframe(df_groups, on_select="rerun", hide_index=True, use_container_width=True)
+    Legge til en ny gruppe
+    """
+)
+with right.form("Legg til gruppe"):
 
-with st.form("Legg til kostnadsstedsrelasjon"):
+    group = st.text_input('Gruppenavn:')
+    group_desc = st.text_input('Gruppebeskrivelse:')
+    submit_group = st.form_submit_button('Lag ny gruppe')
+
+if submit_group:
+        exists_statement = f"""
+            SELECT * FROM grupper WHERE gruppe = upper('{group}') and _slettet_dato is null
+        """
+        df_exists = session.sql(exists_statement).to_pandas()
+        if df_exists.empty:
+            insert_statment=f"""
+                INSERT INTO grupper (
+                    gruppe,
+                    gruppe_beskrivelse,
+                    _opprettet_av,
+                    _opprettet_dato, 
+                    _oppdatert_av,
+                    _oppdatert_dato
+                ) 
+                VALUES (
+                    upper('{group}'),
+                    '{group_desc}',
+                    '{st.experimental_user["email"]}',
+                    current_date, 
+                    '{st.experimental_user["email"]}',
+                    current_date 
+                )"""
+            session.sql(insert_statment).collect()
+            right.success('Success!', icon="✅")
+        else:
+            right.error('Already exists', icon="🚨")
+
+
+right.write(
+    """
+    Slette gruppe
+    """
+)
+with right.form("Slett gruppe"):
     available_groups_statement = f"""
             SELECT gruppe FROM grupper WHERE _slettet_dato is null
         """
     df_available_groups = session.sql(available_groups_statement).to_pandas()
-    available_cost_centre_statement = f"""
-            SELECT kostnadssted FROM gyldig_kostnadssted_liste ORDER BY 1
-        """
-    df_available_cost_centre = session.sql(available_cost_centre_statement).to_pandas()
-    group = st.selectbox("Gyldige grupper",df_available_groups)
-    cost_centre = st.selectbox("Gydlige kostnadssteder",df_available_cost_centre)
-    submit_group = st.form_submit_button('Legg til kostnadsstedsrelasjon')
-    
-if submit_group:
-    exists_statement = f"""
-        SELECT * 
-        FROM gruppe_kostnadssted_relasjoner 
-        WHERE gruppe = upper('{group}') 
-          AND kostnadssted = '{cost_centre}' 
-          AND _slettet_dato is null 
-    """
-    df_exists = session.sql(exists_statement).to_pandas()
-    if df_exists.empty:
-        insert_statment=f"""
-            INSERT INTO gruppe_kostnadssted_relasjoner (
-                gruppe,
-                kostnadssted,
-                _opprettet_av,
-                _opprettet_dato, 
-                _oppdatert_av,
-                _oppdatert_dato
-            ) VALUES (
-                upper('{group}'),
-                '{cost_centre}',
-                '{st.experimental_user["email"]}',
-                current_date, 
-                '{st.experimental_user["email"]}',
-                current_date 
-            )
-        """
-        session.sql(insert_statment).collect()
-        st.success('Success!', icon="✅")    
-    else:
-        st.success('Already exists')
-    # st.rerun()
-
-with st.form("Slett Gruppe"):
-    groups_for_delete_statement = f"""
-            SELECT gruppe 
-            FROM gruppe_kostnadssted_relasjoner 
-            WHERE _slettet_dato is null 
-            ORDER BY 1
-        """
-    df_groups_for_delete = session.sql(groups_for_delete_statement).to_pandas()
-    group = st.selectbox("ledig grupper",df_groups_for_delete)
-    cost_centre_for_delete_statement = f"""
-            SELECT kostnadssted 
-            FROM gruppe_kostnadssted_relasjoner
-            WHERE _slettet_dato is null 
-              AND gruppe = '{group}'
-            ORDER BY 1
-        """
-    df_cost_centre_for_delete = session.sql(cost_centre_for_delete_statement).to_pandas()
-    cost_centre = st.selectbox("ledig kostnadssteder",df_cost_centre_for_delete)
-    delete_group = st.form_submit_button('Slett kostnadsstedsrelasjon')
+    available_groups = [row[0] for row in df_available_groups]
+    group = st.selectbox("Velg gruppe",df_available_groups)
+    delete_group = st.form_submit_button('Slett grupppe')
 
 if delete_group:
     delete_statment=f"""
-        update gruppe_kostnadssted_relasjoner set 
+        update grupper set 
             _oppdatert_av = '{st.experimental_user["email"]}',
             _oppdatert_dato = current_date, 
             _slettet_dato = current_date
         where gruppe = upper('{group}')
-        and kostnadssted = '{cost_centre}'
         """
     session.sql(delete_statment).collect()
-    st.success('Success!', icon="✅")  
-    # st.rerun()
+    right.success('Success!', icon="✅")  
+
+left.write(
+    """
+    Tekst her.
+    """
+)
+
+gruppe_view = f"""SELECT gruppe, gruppe_beskrivelse FROM grupper WHERE _slettet_dato IS NULL"""
+df_groups = session.sql(gruppe_view).to_pandas()
+left.dataframe(df_groups, on_select="rerun", hide_index=True, use_container_width=True)
+
 
