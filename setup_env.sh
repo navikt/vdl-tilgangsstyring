f4a4bdf116a0b04188f4a3a3260da9c919b2d41a
emilieeo <emilie.eliseussen.odegaard@nav.no>
2024-08-19 10:04:20 +0200

    Legge til setup_env script

---
+++ setup_env.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ ! -f .venv/bin/pip ]; then
+  make install
+fi
+
+if [ -f .venv/bin/pip ]; then
+  source .venv/bin/activate
+  dependency_diff=$(grep -Fvf <(pip freeze) requirements-lock.txt)
+fi
+
+if [[ ! -z "$dependency_diff" ]]; then
+  echo deps diff: $dependency_diff
+  make install
+fi
+
+code .
