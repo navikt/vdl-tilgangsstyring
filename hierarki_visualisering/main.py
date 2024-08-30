a75698974e6e5b716e7f4d743579e996b41f13fc
Patrick Pedersen <patrick.pedersen@nav.no>
2024-08-30 13:58:50 +0200

    Vise graphen fra venstre til høyre

--- hierarki_visualisering/main.py
+++ hierarki_visualisering/main.py
@@ -9,7 +9,7 @@ conn = st.connection("snowflake").session()
 
 @st.cache_data
 def get_data():
-    return conn.sql("select * from regnskap.marts.dim_artskonti").to_pandas()
+    return conn.sql("select * from regnskap.marts.dim_artskonti where artskonti_segment_kode_niva_4 is not null").to_pandas()
 
 artskontoer = get_data()
 
@@ -47,9 +47,28 @@ for k, v in display.iterrows():
     edges.add((v["ARTSKONTI_SEGMENT_KODE_NIVA_2"], v["ARTSKONTI_SEGMENT_KODE_NIVA_3"]))
     edges.add((v["ARTSKONTI_SEGMENT_KODE_NIVA_3"], v["ARTSKONTI_SEGMENT_KODE_NIVA_4"]))
 
-graph = graphviz.Digraph()
+graph = graphviz.Digraph(graph_attr={"rankdir": "LR"}, node_attr={"shape": "box"})
 for fra, til in edges:
     graph.edge(fra, til)
 
 with st.container(height=600):
     st.graphviz_chart(graph)
+
+@st.cache_data
+def construct_tree():
+    artskonto_tre = {}
+    for _, v in artskontoer.iterrows():
+        if v["ARTSKONTI_SEGMENT_KODE_NIVA_0"] not in artskonto_tre:
+            artskonto_tre[v["ARTSKONTI_SEGMENT_KODE_NIVA_0"]] = {}
+        if v["ARTSKONTI_SEGMENT_KODE_NIVA_1"] not in artskonto_tre[v["ARTSKONTI_SEGMENT_KODE_NIVA_0"]]:
+            artskonto_tre[v["ARTSKONTI_SEGMENT_KODE_NIVA_0"]][v["ARTSKONTI_SEGMENT_KODE_NIVA_1"]] = {}
+        if v["ARTSKONTI_SEGMENT_KODE_NIVA_2"] not in artskonto_tre[v["ARTSKONTI_SEGMENT_KODE_NIVA_0"]][v["ARTSKONTI_SEGMENT_KODE_NIVA_1"]]:
+            artskonto_tre[v["ARTSKONTI_SEGMENT_KODE_NIVA_0"]][v["ARTSKONTI_SEGMENT_KODE_NIVA_1"]][v["ARTSKONTI_SEGMENT_KODE_NIVA_2"]] = {}
+        if v["ARTSKONTI_SEGMENT_KODE_NIVA_3"] not in artskonto_tre[v["ARTSKONTI_SEGMENT_KODE_NIVA_0"]][v["ARTSKONTI_SEGMENT_KODE_NIVA_1"]][v["ARTSKONTI_SEGMENT_KODE_NIVA_2"]]:
+            artskonto_tre[v["ARTSKONTI_SEGMENT_KODE_NIVA_0"]][v["ARTSKONTI_SEGMENT_KODE_NIVA_1"]][v["ARTSKONTI_SEGMENT_KODE_NIVA_2"]][v["ARTSKONTI_SEGMENT_KODE_NIVA_3"]] = {}
+        if v["ARTSKONTI_SEGMENT_KODE_NIVA_4"] not in artskonto_tre[v["ARTSKONTI_SEGMENT_KODE_NIVA_0"]][v["ARTSKONTI_SEGMENT_KODE_NIVA_1"]][v["ARTSKONTI_SEGMENT_KODE_NIVA_2"]][v["ARTSKONTI_SEGMENT_KODE_NIVA_3"]]:
+            artskonto_tre[v["ARTSKONTI_SEGMENT_KODE_NIVA_0"]][v["ARTSKONTI_SEGMENT_KODE_NIVA_1"]][v["ARTSKONTI_SEGMENT_KODE_NIVA_2"]][v["ARTSKONTI_SEGMENT_KODE_NIVA_3"]][v["ARTSKONTI_SEGMENT_KODE_NIVA_4"]] = {}
+    return artskonto_tre
+
+artskonto_tre = construct_tree()
+st.write(artskonto_tre)
