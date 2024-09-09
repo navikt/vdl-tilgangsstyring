10dc6ceeda18e0f15bc64e97072a2b195209a9bc
emilieeo <emilie.eliseussen.odegaard@nav.no>
2024-09-09 10:57:43 +0200

    Bedre beskrivelse av hvorfor man må klikke 'Oppdater'-knappen

--- app/pages/5_Oppdater_tilganger.py
+++ app/pages/5_Oppdater_tilganger.py
@@ -11,8 +11,8 @@ session = get_active_session()
 check_role(session)
 
 st.title("Oppdater tilganger")
-st.markdown("På denne siden blir tilganger oppdatert og sendt videre til MicroStrategy (MSTR). ")
-st.markdown("Dersom man glemmer å klikke 'Oppdater'-knappen under så vil eventuelle endringer i grupper, kostnadsstedsrelasjoner og oppgaverelasjoner bli lagret i Snowflake, men *ikke* bli sendt til MSTR.")
+st.markdown("Trykk 'Oppdater' for å oppdatere tilganger til MicroStrategy (MSTR). ")
+st.markdown("Merk: dersom man ikke klikker 'Oppdater' under så vil eventuelle endringer i grupper, kostnadsstedsrelasjoner og oppgaverelasjoner bli lagret i Snowflake, men *ikke* bli sendt til MSTR.")
 
 
 update_data = st.button("Oppdater")
