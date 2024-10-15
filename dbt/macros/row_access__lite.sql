851f569dfe382f7930181be3d4cec05693bd1515
Ivar Framnes <ivar.william.kvale.framnes@nav.no>
2024-10-15 12:21:12 +0200

    bedre tilgangsstyring

--- dbt/macros/row_access__lite.sql
+++ dbt/macros/row_access__lite.sql
@@ -1,13 +1,47 @@
 {% macro row_access__lite(policy_db, policy_schema) %}
     {% set body %}
-         exists (
-            select 1 
-            from tilgangsstyring.policies.bruker_tilganger__filtrering 
-            where login_navn = current_user() 
-            and (kostnadssted = kostnadssted_kode or kostnadssted_gruppe = 'TOTAL')
-            and (oppgave = oppgave_kode or oppgave_gruppe = 'TOTAL')
-            and (artskonto = artskonto_kode or artskonto_gruppe = 'TOTAL')
-            ) or current_role() like '%_TRANSFORMER' or current_role() like '%_LOADER'
+         (
+            (
+                (
+                    select max(har_all_kostnadssteder) 
+                    from tilgangsstyring.policies.bruker_tilganger__filtrering 
+                    where login_navn = current_user
+                ) = 1 
+                or contains(
+                    (
+                        select max(kostnadssteder) 
+                        from tilgangsstyring.policies.bruker_tilganger__filtrering 
+                        where login_navn = current_user
+                    ), kostnadssted_kode
+                )
+            ) and (
+                (
+                    select max(har_all_oppgaver) 
+                    from tilgangsstyring.policies.bruker_tilganger__filtrering 
+                    where login_navn = current_user
+                ) = 1 
+                or contains(
+                    (
+                        select max(oppgaver) 
+                        from tilgangsstyring.policies.bruker_tilganger__filtrering 
+                        where login_navn = current_user
+                    ), oppgave_kode
+                )
+            ) and (
+                (
+                    select max(har_all_artskonti) 
+                    from tilgangsstyring.policies.bruker_tilganger__filtrering 
+                    where login_navn = current_user
+                ) = 1 
+                or contains(
+                    (
+                        select max(artskonti) 
+                        from tilgangsstyring.policies.bruker_tilganger__filtrering 
+                        where login_navn = current_user
+                    ), artskonto_kode
+                )
+            )
+        ) or current_role() like '%_TRANSFORMER' or current_role() like '%_LOADER'
     {% endset %}
 
     {% do vdl_macros.create_row_access_policy(
